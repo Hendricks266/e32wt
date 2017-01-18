4016406d335649e90ea9340a42d68f8cba2f1edd
Evan Ramos <Hendricks266@gmail.com>
2017-01-18 16:28:33 -0600

    Add package script

---
+++ package.bat
@@ -0,0 +1,5 @@
+@echo off
+
+for /f "delims=" %%G in ('grep Release readme_e32wt.txt ^| cut -d " " -f 2') do @set releasedate=%%G
+
+7z.exe a -mx9 -t7z "e32wt_%releasedate%.7z" *.CON *.def *.grpinfo *.txt
