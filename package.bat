48a248a2a8d3a0779d9e0216504e085888c549ad
Evan Ramos <Hendricks266@gmail.com>
2017-04-09 08:26:39 -0500

    Update package script to include GRP files

--- package.bat
+++ package.bat
@@ -2,4 +2,4 @@
 
 for /f "delims=" %%G in ('grep Release readme_e32wt.txt ^| cut -d " " -f 2') do @set releasedate=%%G
 
-7z.exe a -mx9 -t7z "e32wt_%releasedate%.7z" *.CON *.def *.grpinfo *.txt
+7z.exe a -mx9 -t7z "e32wt_%releasedate%.7z" *.CON *.def *.grpinfo *.txt *.grp
