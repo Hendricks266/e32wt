e145eb988c6c3ea2803aee5e95080586a8f8951f
Evan Ramos <Hendricks266@gmail.com>
2019-06-27 22:42:08 -0500

    Split documentation into README.md and version_e32wt.txt

--- package.bat
+++ package.bat
@@ -1,9 +1,9 @@
 @echo off
 PATH=%PATH%;C:\Program Files\7-Zip;C:\msys64\usr\bin
 
-for /f "delims=" %%G in ('grep Release readme_e32wt.txt ^| cut -d " " -f 2') do @set releasedate=%%G
+for /f "delims=" %%G in ('grep Release version_e32wt.txt ^| cut -d " " -f 2') do @set releasedate=%%G
 
-set files=*.CON *.def *.grpinfo *.txt *.grp
+set files=*.CON *.def *.grpinfo *.txt *.grp *.md
 :: 7z.exe a -mx9 -t7z "e32wt_%releasedate%.7z" %files%
 :: 7z.exe a -mx9 -tzip "e32wt_%releasedate%.zip" %files%
 kzip.exe "e32wt_%releasedate%.zip" %files%
